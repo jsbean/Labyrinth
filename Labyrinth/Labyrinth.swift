5db382a38230cb45a66e0e0956b6698ef65a287e
jsbean <jbean46@gmail.com>
2016-09-22 15:46:23 -0400

    Remove unused center proportion

--- Labyrinth/Labyrinth.swift
+++ Labyrinth/Labyrinth.swift
@@ -10,12 +10,9 @@ import Foundation
 
 struct Labyrinth {
     
-    private let centerProportion: Float // 0...1
-    
     let circuits: [Circuit]
     
     init(amountCircuits: UInt, centerProportion: Float = 0.25) {
-        self.centerProportion = centerProportion
         let circuitWidth = (1.0 - centerProportion) / (Float(amountCircuits - 1))
         self.circuits = (0 ..< amountCircuits).map { i in
             return Circuit(index: i, diameter: 1.0 - Float(i) * circuitWidth)
